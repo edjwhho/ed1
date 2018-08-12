e34670596d8ed37fe276b1958f9625a0cf5cfe33
edj <edjwh.ho@gmail.com>
2018-08-12 22:48:10 +0100

    added stuff

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,9 @@
 # Create VPC/Subnet/Security Group/ACL
 
 provider "aws" {
-        region     = "${var.region}"
-} # end provider
+  region = "us-east-1"
+}
+
 
 # create the VPC
 resource "aws_vpc" "DEV_TEST" {
@@ -33,12 +34,22 @@ resource "aws_security_group" "DEV_TEST_Security_Group" {
   vpc_id       = "${aws_vpc.DEV_TEST.id}"
   name         = "DEV TEST Security Group"
   description  = "DEV TEST Security Group"
+
 ingress {
     cidr_blocks = "${var.ingressCIDRblock}"  
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
   }
+
+  # HTTP access from the VPC
+  ingress {
+    cidr_blocks = "${var.ingressCIDRblock}"  
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+
 	tags = {
         Name = "DEV TEST Security Group"
   }
@@ -64,8 +75,10 @@ resource "aws_network_acl" "DEV_TEST_Security_ACL" {
 	    	rule_no    = 200
     		action     = "allow"
     		cidr_block = "${var.destinationCIDRblock}"
-    		from_port  = 1024
-    		to_port    = 65535
+    		from_port  = 8080
+    		to_port    = 8080
+    		#from_port  = 1024
+    		#to_port    = 65535
   		}
 	# allow egress ephemeral ports
   	egress {
@@ -105,9 +118,55 @@ resource "aws_route" "DEV_TEST_internet_access" {
 } # end resource
 
 # Associate the Route Table with the Subnet
-resource "aws_route_table_association" "DEV_TEST_association" {
-    subnet_id      = "${aws_subnet.DEV_TEST_Subnet.id}"
-    route_table_id = "${aws_route_table.DEV_TEST_route_table.id}"
-} # end resource
+
+# resource "aws_route_table_association" "DEV_TEST_association" {
+#    subnet_id      = "${aws_subnet.DEV_TEST_Subnet.id}"
+#    route_table_id = "${aws_route_table.DEV_TEST_route_table.id}"
+# } # end resource
 
 # end vpc.tf
+
+
+#resource "aws_key_pair" "auth" {
+#  key_name   = "${var.key_name}"
+#  public_key = "${file(var.public_key_path)}"
+#}
+
+
+resource "aws_instance" "testserver1" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  ami = "ami-2d39803a"
+  instance_type = "t2.micro"
+
+  # The name of our SSH keypair we created above.
+#  key_name = "${aws_key_pair.auth.id}"
+
+  #our Security group to allow HTTP and SSH access
+  #vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = ["${aws_security_group.DEV_TEST_Security_Group.id}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  #subnet_id = "${aws_subnet.default.id}"
+  subnet_id = "${aws_subnet.DEV_TEST_Subnet.id}"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags {
+
+    Name = "terraform_test"
+  }
+}
+
